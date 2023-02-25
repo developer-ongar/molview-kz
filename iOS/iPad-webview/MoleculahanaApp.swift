9b032e6dbc2c1ed64020d9f669d04d0af4113a85
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-02-25 16:36:06 +0600

    Create   MoleculahanaApp.swift

---
+++ iOS/iPad-webview/MoleculahanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoleculahanaApp.swift
+//  Moleculahana
+//
+//  Created by Ongar.dev on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoleculahanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
